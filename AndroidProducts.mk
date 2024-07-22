55a3fb97c8a5e6e853775abc4f872d93d3c08101
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-22 22:45:56 +0200

    moon: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: moon Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_moon.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_moon-user \
+    omni_moon-userdebug \
+    omni_moon-eng
